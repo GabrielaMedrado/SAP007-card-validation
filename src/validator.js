86b78023900f468cdbeb586e8fbb44a7b239259f
Gabriela Medrado <gabrieladmr@gmail.com>
2022-01-23 14:22:11 -0300

    Verificação numérica

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,28 @@
 const validator = {
   // ...
+  isValid(creditCardNumber) {
+
+    //verifica se foi digitado somente números 
+    for (var i = 0; i < creditCardNumber.length; i++) {
+      console.log(creditCardNumber.charAt(i));
+      if (isNaN(creditCardNumber.charAt(i)) || creditCardNumber.charAt(i) == " ") {
+        console.log("Cartão Inválido!")
+        return "Cartão Inválido";
+      }
+    }
+
+    // algoritmo de Luhn
+    for (var i = 0;  )
+
+
+
+
+
+  },
+  maskify(creditCardNumber) {
+
+  }
+
 };
 
 export default validator;
